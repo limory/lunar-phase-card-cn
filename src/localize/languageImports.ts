01cf41c9b0b0bed21e209ed94d72110cf9e8622f
limory <limory@qq.com>
2025-05-18 23:09:50 +0800

    Leaving only cn and en,other languages have been removed.

--- src/localize/languageImports.ts
+++ src/localize/languageImports.ts
@@ -1,61 +1,17 @@
 // This file is generated automatically by the generate-lang-imports script. Do not modify it manually.
 
-import * as ca from '../languages/ca.json';
 import * as cn from '../languages/cn.json';
-import * as cs from '../languages/cs.json';
-import * as da from '../languages/da.json';
-import * as de from '../languages/de.json';
 import * as en from '../languages/en.json';
-import * as es from '../languages/es.json';
-import * as fr from '../languages/fr.json';
-import * as id from '../languages/id.json';
-import * as it from '../languages/it.json';
-import * as lt from '../languages/lt.json';
-import * as nl from '../languages/nl.json';
-import * as pl from '../languages/pl.json';
-import * as pt from '../languages/pt.json';
-import * as ru from '../languages/ru.json';
-import * as sk from '../languages/sk.json';
-import * as vi from '../languages/vi.json';
+
 
 const languages: any = {
-  ca: ca,
   cn: cn,
-  cs: cs,
-  da: da,
-  de: de,
   en: en,
-  es: es,
-  fr: fr,
-  id: id,
-  it: it,
-  lt: lt,
-  nl: nl,
-  pl: pl,
-  pt: pt,
-  ru: ru,
-  sk: sk,
-  vi: vi,
 };
 
 export const languageOptions = [
-  { key: 'ca', name: ca.name, nativeName: ca.nativeName },
   { key: 'cn', name: cn.name, nativeName: cn.nativeName },
-  { key: 'cs', name: cs.name, nativeName: cs.nativeName },
-  { key: 'da', name: da.name, nativeName: da.nativeName },
-  { key: 'de', name: de.name, nativeName: de.nativeName },
   { key: 'en', name: en.name, nativeName: en.nativeName },
-  { key: 'es', name: es.name, nativeName: es.nativeName },
-  { key: 'fr', name: fr.name, nativeName: fr.nativeName },
-  { key: 'id', name: id.name, nativeName: id.nativeName },
-  { key: 'it', name: it.name, nativeName: it.nativeName },
-  { key: 'lt', name: lt.name, nativeName: lt.nativeName },
-  { key: 'nl', name: nl.name, nativeName: nl.nativeName },
-  { key: 'pl', name: pl.name, nativeName: pl.nativeName },
-  { key: 'pt', name: pt.name, nativeName: pt.nativeName },
-  { key: 'ru', name: ru.name, nativeName: ru.nativeName },
-  { key: 'sk', name: sk.name, nativeName: sk.nativeName },
-  { key: 'vi', name: vi.name, nativeName: vi.nativeName },
 ];
 
 export { languages };
